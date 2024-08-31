88eccad380d25e377e8a85f975c174e1c2fa1850
liukaixin <15713857919@163.com>
2024-08-31 23:26:44 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const a = 1;
+    console.log("a", a);
+  }, []);
   return <h2>webpack5-react-ts2</h2>;
 }
 export default App;
